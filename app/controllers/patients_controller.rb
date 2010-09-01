85b35d782672fee505b0348ca6533c8154233da2
Jesús Mercado <jesusmmm@gmail.com>
2010-08-31 23:07:53 -0400

    Configuración de Patient ActiveScaffold - Configuración de los campos agrupados por categorias

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -2,15 +2,26 @@ class PatientsController < ApplicationController
   active_scaffold :patient do |conf|
     #Configuración de las columnas que se mostrarán al listar
     conf.list.columns = [:last_name, :first_name]
+
     #Configuración de las acciones que se mostrarán
     conf.actions = [:create, :search, :update, :delete, :show, :nested, :subform, :list]
-    #Configuración del orden en que se muestran los campos al crear
-    #conf.create.columns = [:first_name, :last_name, :date_of_birth, :origin, :mother, :father, :address, :phone_number, :referenced_by, :observations]
-    conf.create.columns = [:date_of_birth, :origin, :mother, :father, :address, :phone_number, :referenced_by, :observations]
+
+    #Configuración de los campos agrupados por categorias
+    conf.create.columns.add_subgroup "Personal data" do |personal_data_group|
+      personal_data_group.add :first_name, :last_name, :date_of_birth, :origin
+    end
+    conf.create.columns.add_subgroup "Parents data" do |parents_data_group|
+      parents_data_group.add :mother, :father, :address, :phone_number, :contact_emails
+    end
+    conf.create.columns.add_subgroup "Medical data" do |medical_data_group|
+      medical_data_group.add :referenced_by, :observations
+    end
+
     #La fecha se muestra con un calendario
     conf.columns[:date_of_birth].form_ui = :calendar
+
     #Las observaciones se muestran con un textarea de 10x50
-      conf.columns[:observations].form_ui = :textarea
-      conf.columns[:observations].options = {:rows => 10, :cols => 50 }
+    conf.columns[:observations].form_ui = :textarea
+    conf.columns[:observations].options = {:rows => 10, :cols => 50 }
   end
-end 
+end
